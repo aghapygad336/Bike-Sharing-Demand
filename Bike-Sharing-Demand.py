6709f575fda6e8acf289745e399b16a22f158655
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-09-17 23:47:10 +0200

    Normalization is found

---
+++ Bike-Sharing-Demand.py
@@ -0,0 +1,39 @@
+import pandas
+import numpy
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+# load data
+import csv
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+
+# Feature Extraction with Univariate Statistical Tests (Chi-squared for classification)
+
+
+date_column = ("datetime")
+f = open("test.csv","r")
+csv_reader = csv.reader(f)
+
+headers = None
+results = []
+for data in csv_reader:
+    if not headers:
+        headers = []
+        for i, col in enumerate(data):
+           if col in date_column:
+            headers.append(i)
+
+    else:
+        results.append(([data[i] for i in headers]))
+
+        print("$$$$$$$$$$$$$")
+
+
+print("^^^^^^^^^^^^^")
+# current date and time
+now = datetime.now()
+timestamp = datetime.timestamp(now)
+print("timestamp =", timestamp)
+print(now)
